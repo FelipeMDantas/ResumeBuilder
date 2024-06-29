72e444ecbac7278669892dcab5ff76c0fe77d345
FelipeMDantas <felipemdantas@terra.com.br>
2024-06-28 23:47:59 -0300

    Dialog and Input components added to the project.

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -1,5 +1,15 @@
+import AddResume from "./components/AddResume";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="p-10 md:px-20 lg:px-32">
+      <h2 className="font-bold text-3xl">My Resume</h2>
+      <p>Create an AI generated resume for your next role</p>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mt-10">
+        <AddResume />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
