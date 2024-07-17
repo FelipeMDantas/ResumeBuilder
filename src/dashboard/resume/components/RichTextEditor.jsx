c7ce92317a3246f8dfb6a623028f450be1e22316
FelipeMDantas <felipemdantas@terra.com.br>
2024-07-16 21:52:26 -0300

    RichTextEditor component created.

---
+++ src/dashboard/resume/components/RichTextEditor.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { Editor, EditorProvider } from "react-simple-wysiwyg";
+
+const RichTextEditor = () => {
+  const [value, setValue] = useState();
+
+  return (
+    <div>
+      <EditorProvider>
+        <Editor
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        ></Editor>
+      </EditorProvider>
+    </div>
+  );
+};
+
+export default RichTextEditor;
--- src/dashboard/resume/components/forms/Experience.jsx
+++ src/dashboard/resume/components/forms/Experience.jsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
+import RichTextEditor from "../RichTextEditor";
 
 const formField = {
   title: "",
@@ -21,57 +23,68 @@ const Experience = () => {
       <div className="p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10">
         <div className="font-bold text-lg">Professional Experience</div>
         <p>Add your previous job experience</p>
-      </div>
-      {exprienceList.map((item, index) => (
         <div>
-          <div className="grid grid-cols-2 gap-3 border p-3 my-5 rounded-lg">
-            <div>
-              <label className="text-xs">Position Title</label>
-              <Input
-                name="title"
-                onChange={(event) => handleChange(index, event)}
-              />
+          {exprienceList.map((item, index) => (
+            <div key={index}>
+              <div className="grid grid-cols-2 gap-3 border p-3 my-5 rounded-lg">
+                <div>
+                  <label className="text-xs">Position Title</label>
+                  <Input
+                    name="title"
+                    onChange={(event) => handleChange(index, event)}
+                  />
+                </div>
+                <div>
+                  <label className="text-xs">Company Name</label>
+                  <Input
+                    name="companyName"
+                    onChange={(event) => handleChange(index, event)}
+                  />
+                </div>
+                <div>
+                  <label className="text-xs">City</label>
+                  <Input
+                    name="city"
+                    onChange={(event) => handleChange(index, event)}
+                  />
+                </div>
+                <div>
+                  <label className="text-xs">State</label>
+                  <Input
+                    name="state"
+                    onChange={(event) => handleChange(index, event)}
+                  />
+                </div>
+                <div>
+                  <label className="text-xs">Start Date</label>
+                  <Input
+                    name="startDate"
+                    onChange={(event) => handleChange(index, event)}
+                    type="date"
+                  />
+                </div>
+                <div>
+                  <label className="text-xs">End Date</label>
+                  <Input
+                    name="endDate"
+                    onChange={(event) => handleChange(index, event)}
+                    type="date"
+                  />
+                </div>
+                <div className="col-span-2">
+                  <RichTextEditor />
+                </div>
+              </div>
             </div>
-            <div>
-              <label className="text-xs">Company Name</label>
-              <Input
-                name="companyName"
-                onChange={(event) => handleChange(index, event)}
-              />
-            </div>
-            <div>
-              <label className="text-xs">City</label>
-              <Input
-                name="city"
-                onChange={(event) => handleChange(index, event)}
-              />
-            </div>
-            <div>
-              <label className="text-xs">State</label>
-              <Input
-                name="state"
-                onChange={(event) => handleChange(index, event)}
-              />
-            </div>
-            <div>
-              <label className="text-xs">Start Date</label>
-              <Input
-                name="startDate"
-                onChange={(event) => handleChange(index, event)}
-                type="date"
-              />
-            </div>
-            <div>
-              <label className="text-xs">End Date</label>
-              <Input
-                name="endDate"
-                onChange={(event) => handleChange(index, event)}
-                type="date"
-              />
-            </div>
-          </div>
+          ))}
         </div>
-      ))}
+        <div className="flex justify-between">
+          <Button variant="outline" className="text-primary">
+            + Add Experience
+          </Button>
+          <Button>Save</Button>
+        </div>
+      </div>
     </div>
   );
 };
