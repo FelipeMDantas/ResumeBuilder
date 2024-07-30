a36beefb626bb5770019e4a2c950e30723b16ef7
FelipeMDantas <felipemdantas@terra.com.br>
2024-07-29 23:50:37 -0300

    Resume view further stylized.

--- src/dashboard/my-resume/[resumeId]/view/index.jsx
+++ src/dashboard/my-resume/[resumeId]/view/index.jsx
@@ -27,23 +27,25 @@ const ViewResume = () => {
 
   return (
     <ResumeInfoContext.Provider value={{ resumeInfo, setResumeInfo }}>
-      <Header />
+      <div id="no-print">
+        <Header />
+        <div className="my-10 mx-10 md:mx-20 lg:mx-36">
+          <h2 className="text-center text-2xl font-medium">
+            Congrats! Your AI generated resume is ready!
+          </h2>
+          <p className="text-center text-gray-400">
+            Now you are ready to download your resume and share a unique URL
+            with your friends and family.
+          </p>
+          <div className="flex justify-between px-44 my-10">
+            <Button onClick={handleDownload}>Download</Button>
+            <Button>Share</Button>
+          </div>
+        </div>
+      </div>
 
-      <div className="mt-10 mx-10 md:mx-20 lg:mx-36">
-        <h2 className="text-center text-2xl font-medium">
-          Congrats! Your AI generated resume is ready!
-        </h2>
-        <p className="text-center text-gray-400">
-          Now you are ready to download your resume and share a unique URL with
-          your friends and family.
-        </p>
-        <div className="flex justify-between px-44 my-10">
-          <Button onClick={handleDownload}>Download</Button>
-          <Button>Share</Button>
-        </div>
-        <div>
-          <ResumePreview />
-        </div>
+      <div id="print-area" className="my-10 mx-10 md:mx-20 lg:mx-36">
+        <ResumePreview />
       </div>
     </ResumeInfoContext.Provider>
   );
