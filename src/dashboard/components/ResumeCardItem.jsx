236a4806e19823bd976e6435dd29c6f8d23f387e
FelipeMDantas <felipemdantas@terra.com.br>
2024-07-31 23:53:51 -0300

    ResumeCardItem component further developed..

--- src/dashboard/components/ResumeCardItem.jsx
+++ src/dashboard/components/ResumeCardItem.jsx
@@ -1,13 +1,30 @@
 import { MoreVertical } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { useState } from "react";
 
 const ResumeCardItem = ({ resume }) => {
+  const navigation = useNavigate();
+
+  const [openAlert, setOpenAlert] = useState(false);
+
+  const onDelete = () => {}
+
   return (
     <>
       <Link to={`/dashboard/resume/${resume.documentId}/edit`}>
@@ -33,12 +50,46 @@ const ResumeCardItem = ({ resume }) => {
             <MoreVertical className="h-4 w-4 cursor-pointer" />
           </DropdownMenuTrigger>
           <DropdownMenuContent>
-            <DropdownMenuItem>Edit</DropdownMenuItem>
-            <DropdownMenuItem>View</DropdownMenuItem>
-            <DropdownMenuItem>Download</DropdownMenuItem>
-            <DropdownMenuItem>Delete</DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() =>
+                navigation(`/dashboard/resume/${resume.documentId}/edit`)
+              }
+            >
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() => navigation(`/my-resume/${resume.documentId}/view`)}
+            >
+              View
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() => navigation(`/my-resume/${resume.documentId}/view`)}
+            >
+              Download
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setOpenAlert(true)}>
+              Delete
+            </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
+
+        <AlertDialog open={openAlert}>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel onClick={() => setOpenAlert(false)}>
+                Cancel
+              </AlertDialogCancel>
+              <AlertDialogAction onClick={onDelete}>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </div>
     </>
   );
